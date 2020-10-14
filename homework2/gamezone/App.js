fa5c1c5c9ee75d00eb4f516400adb689ea256a58
ridgedejong <rad47@students.calvin.edu>
2020-10-14 12:03:43 -0400

    delete unused code

--- homework2/gamezone/App.js
+++ homework2/gamezone/App.js
@@ -32,32 +32,4 @@ export default function App() {
 
 }
 
-// import React, { useEffect, useState } from 'react';
-// import { ActivityIndicator, FlatList, Text, View } from 'react-native';
 
-// export default function App() {
-//   const [isLoading, setLoading] = useState(true);
-//   const [data, setData] = useState([]);
-
-//   useEffect(() => {
-//     fetch('https://cs262-monopoly-service.herokuapp.com/players/')
-//         .then((response) => response.json())
-//         .then((json) => setData(json))
-//         .catch((error) => console.error(error))
-//         .finally(() => setLoading(false));
-//   }, []);
-
-//   return (
-//       <View style={{ flex: 1, padding: 24 }}>
-//         {isLoading ? <ActivityIndicator/> : (
-//             <FlatList
-//                 data={data}
-//                 keyExtractor={({ id }, index) => id.toString()}
-//                 renderItem={({ item }) => (
-//                     <Text>{item.id}, {item.email}, {item.name}</Text>
-//                 )}
-//             />
-//         )}
-//       </View>
-//   );
-// };
--- homework2/gamezone/screens/home.js
+++ homework2/gamezone/screens/home.js
@@ -4,11 +4,6 @@ import { globalStyles } from '../styles/global';
 import Card from '../shared/card';
 
 export default function Home({ navigation }) {
-    // const [reviews, setReviews] = useState([
-    //     { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
-    //     { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
-    //     { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' },
-    // ]);
 
     const [isLoading, setLoading] = useState(true);
     const [data, setData] = useState([]);
@@ -39,15 +34,4 @@ export default function Home({ navigation }) {
         </View>
     );
 
-    // return (
-    //     <View style={globalStyles.container}>
-    //         <FlatList data={reviews} renderItem={({ item }) => (
-    //             <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
-    //                 <Card>
-    //                     <Text style={globalStyles.titleText}>{ item.title }</Text>
-    //                 </Card>
-    //             </TouchableOpacity>
-    //         )} />
-    //     </View>
-    // );
 }
